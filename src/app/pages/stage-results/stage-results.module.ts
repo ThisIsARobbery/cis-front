50ca80f88252d51c5ac237b34cbfd2ffc6c7836f
Gordey.Babich <Gordey.Babich@x5.ru>
2021-09-03 00:53:11 +0300

    added stage-results page and form

--- src/app/pages/stage-results/stage-results.module.ts
+++ src/app/pages/stage-results/stage-results.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
 
 import { StageResultsRoutingModule } from './stage-results-routing.module';
 import { StageResultsComponent } from './stage-results/stage-results.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
 
 
 @NgModule({
@@ -13,8 +18,13 @@ import { StageResultsComponent } from './stage-results/stage-results.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     StageResultsRoutingModule,
-    NzBreadCrumbModule
+    NzBreadCrumbModule,
+    NzCollapseModule,
+    NzInputModule,
+    NzButtonModule,
+    NzFormModule
   ]
 })
 export class StageResultsModule { }
--- src/app/pages/stage-results/stage-results/stage-results.component.ts
+++ src/app/pages/stage-results/stage-results/stage-results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './stage-results.component.html',
@@ -6,9 +7,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StageResultsComponent implements OnInit {
 
-  constructor() { }
+  testData = [
+    {
+      number: 1,
+      title: 'Stage 1',
+      description: 'Stage 1 description blah blah blah',
+      actor: 'test actor',
+      isConditional: false,
+      priority: 'low',
+      active: true,
+    },
+    {
+      number: 2,
+      title: 'Stage 2',
+      description: 'Stage 2 description blah blah blah',
+      actor: 'test actor 2',
+      isConditional: false,
+      priority: 'medium',
+      active: false,
+    },
+    {
+      number: 3,
+      title: 'Stage 3',
+      description: 'Stage 13description blah blah blah',
+      actor: 'test actor',
+      isConditional: false,
+      priority: 'high',
+      active: false,
+    },
+    {
+      number: 4,
+      title: 'Stage 4',
+      description: 'Stage 4 description blah blah blah',
+      actor: 'test actor',
+      isConditional: false,
+      priority: 'low',
+      active: false,
+    },
+  ];
+
+  currentStage = {
+    number: 5,
+    title: 'Stage 5',
+    description: 'Stage 5 description',
+    actor: 'test actor',
+    isConditional: false,
+    priority: 'high'
+  };
+
+  currentStageForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.currentStageForm = this.fb.group({
+      note: null,
+    });
+  }
+
+  submitCurrentStage(): void {
+    for (const i in this.currentStageForm.controls) {
+      if (this.currentStageForm.controls.hasOwnProperty(i)) {
+        this.currentStageForm.controls[i].markAsDirty();
+        this.currentStageForm.controls[i].updateValueAndValidity();
+      }
+    }
   }
 
 }
